084352c9c9de3513047de57d6d6d37b8c8e050c0
Alexandr Pushylo <alexandr.pushylo@gmail.com>
2024-05-29 23:18:31 +0300

    ref models

--- debt_manager/models.py
+++ debt_manager/models.py
@@ -1,13 +1,51 @@
 from django.db import models
-from m_hub.assets import CURRENCY_CHOICES
+from debt_manager.assets import CURRENCY_CHOICES
 
 # Create your models here.
 
 
+class Debtor(models.Model):
+    first_name = models.CharField(max_length=120, verbose_name='Имя')
+    last_name = models.CharField(max_length=120, verbose_name='Фамилия')
+    solvency = models.DecimalField(max_digits=4, decimal_places=2, verbose_name='Платежеспособность', default=0)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+    class Meta:
+        verbose_name = 'Должник'
+        verbose_name_plural = 'Должники'
+        ordering = ['last_name']
+
+
 class Debt(models.Model):
-    title = models.CharField(max_length=256, verbose_name='Название')
+    title = models.CharField(max_length=256, verbose_name='Название', null=True, blank=True)
     description = models.TextField(verbose_name='Описание', blank=True, null=True)
     amount = models.DecimalField(max_digits=9, decimal_places=2, verbose_name='Сумма', blank=True, null=True)
     currency = models.CharField(max_length=200, verbose_name='Валюта', choices=CURRENCY_CHOICES)
-    date_start = models.DateField(verbose_name='Дата создания', blank=True, null=True)
+    date_start = models.DateTimeField(verbose_name='Дата создания', blank=True, null=True)
     date_end = models.DateField(verbose_name='Конечная дата', blank=True, null=True)
+    debtors = models.ForeignKey(Debtor, on_delete=models.CASCADE, verbose_name='Должник')
+    is_closed = models.BooleanField(default=False, verbose_name='Закрыто')
+
+    def __str__(self):
+        return f"{self.debtors} - {self.amount} {self.currency} - {'Открыто' if self.is_closed else 'Закрыто'}"
+
+    class Meta:
+        verbose_name = 'Долг'
+        verbose_name_plural = 'Долги'
+        ordering = ['date_end']
+
+
+class ExchangeRate(models.Model):
+    rate = models.DecimalField(max_digits=7, decimal_places=2, verbose_name='Курс')
+    date = models.DateField(verbose_name='Дата')
+    currency = models.CharField(max_length=200, verbose_name='Валюта', choices=CURRENCY_CHOICES)
+
+    def __str__(self):
+        return f'{self.date} - {self.rate} {self.currency}'
+
+    class Meta:
+        verbose_name = 'Курс'
+        verbose_name_plural = 'Курсы'
+        ordering = ['date']
