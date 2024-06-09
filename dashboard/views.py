61cd548ee2ce7c3c5271c5511f694061e17faebf
Alexandr Pushylo <alexandr.pushylo@gmail.com>
2024-06-09 22:19:51 +0300

    added bells card

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from debt_manager.models import Debt, Debtor, ExchangeRate
 from debt_manager import utilites as U
+from bills.models import Electricity, ColdWater, HotWater, Rent, Gas
 
 # Create your views here.
 
@@ -13,6 +14,18 @@ def dashboard(request):
     debtor_list = debtors.values()
     for debtor in debtor_list:
         debtor['debts'] = debt_list.filter(debtor_id=debtor['id'])
-
     out['debtor_list'] = debtor_list
+
+    electricity_list = Electricity.objects.filter(payment_date__month=U.MONTH)
+    cold_water_list = ColdWater.objects.filter(payment_date__month=U.MONTH)
+    hot_water_list = HotWater.objects.filter(payment_date__month=U.MONTH)
+    rent_list = Rent.objects.filter(payment_date__month=U.MONTH)
+    gas_list = Gas.objects.filter(payment_date__month=U.MONTH)
+
+    out['electricity_list'] = electricity_list
+    out['cold_water_list'] = cold_water_list
+    out['hot_water_list'] = hot_water_list
+    out['rent_list'] = rent_list
+    out['gas_list'] = gas_list
+
     return render(request, 'dashboard/dashboard.html', out)
--- debt_manager/utilites.py
+++ debt_manager/utilites.py
@@ -3,6 +3,7 @@ from datetime import date, datetime
 
 TODAY = date.today
 NOW = datetime.now().time
+MONTH = datetime.now().month
 
 
 def get_transaction_count(debtor: Debtor) -> int:
