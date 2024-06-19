5042e87613208f607970747b30c2bf3d61478ea9
Alexandr Pushylo <alexandr.pushylo@gmail.com>
2024-06-19 21:54:27 +0300

    add edit_hot_water

--- bills/urls.py
+++ bills/urls.py
@@ -4,4 +4,5 @@ from bills.views import edit_electricity, edit_cold_water, edit_hot_water, edit_
 urlpatterns = [
     path('edit_electricity/', edit_electricity, name='edit_electricity'),
     path('edit_cold_water/', edit_cold_water, name='edit_cold_water'),
+    path('edit_hot_water/', edit_hot_water, name='edit_hot_water'),
 ]
--- bills/views.py
+++ bills/views.py
@@ -107,4 +107,54 @@ def edit_cold_water(request):
 
     return render(request, template, context)
 
+
+def edit_hot_water(request):
+    template = 'bills/edit_hot_water.html'
+    context = {
+        'title': 'Горячая вода'
+    }
+    last_hot_water = HotWater.objects.filter(payment_date__month=U.MONTH - 1)
+    current_hot_water = HotWater.objects.filter(payment_date__month=U.MONTH)
+
+    if request.method == 'POST':
+        indications = request.POST.get('current_indications')
+        amount = request.POST.get('amount')
+        rate = request.POST.get('rate')
+        payment_date = request.POST.get('payment_date', U.TODAY)
+        description = request.POST.get('description')
+
+        if all([indications, amount, rate, payment_date]):
+            electricity, _ = HotWater.objects.update_or_create(payment_date__month=U.MONTH, defaults={
+                'indications': indications,
+                'amount': amount,
+                'rate': rate,
+                'payment_date': payment_date,
+                'description': description
+            })
+        return HttpResponseRedirect(ENDPOINTS.DASHBOARD)
+    if last_hot_water.exists():
+        rate = last_hot_water.first().rate
+        last_indications = last_hot_water.first().indications
+    else:
+        rate = 0
+        last_indications = 0
+
+    if current_hot_water.exists():
+        current_indications = current_hot_water.first().indications
+        description = current_hot_water.first().description
+        amount = current_hot_water.first().amount
+        payment_date = current_hot_water.first().payment_date
+
+        context['amount'] = str(amount).replace(',', '.')
+        context['description'] = description
+        context['current_indications'] = current_indications
+        context['indications_diff'] = current_indications - last_indications
+    else:
+        payment_date = U.TODAY
+
+    context['payment_date'] = payment_date
+    context['last_indications'] = last_indications
+    context['rate'] = str(rate).replace(',', '.')
+
+    return render(request, template, context)
     return render(request, template, context)
